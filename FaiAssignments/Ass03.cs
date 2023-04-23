6ff4f10e4f79a8646e3d1c1ea3f0e1e3044ec17a
kandysh <rajawat.k.029@gmail.com>
2023-04-23 19:28:19 +0530

    second

---
+++ FaiAssignments/Ass03.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAIDotnetAssignment
+{
+    class Ass03
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
